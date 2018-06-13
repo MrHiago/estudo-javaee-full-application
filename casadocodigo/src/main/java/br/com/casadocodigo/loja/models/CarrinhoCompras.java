e88eb5da70cebd5af3d8b52f8c0e253b40da1d3f
Hiago Miranda Ribeiro <hiagomiranda95@gmail.com>
2018-06-13 19:32:19 -0300

    adicionando logica para o valor individual dos itens no carrinho e no total dos itens do carrinho

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -1,6 +1,7 @@
 package br.com.casadocodigo.loja.models;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -11,12 +12,12 @@ import javax.inject.Named;
 
 @Named
 @SessionScoped
-public class CarrinhoCompras implements Serializable{
+public class CarrinhoCompras implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Set<CarrinhoItem> itens = new HashSet<>();
-	
+
 	public void add(CarrinhoItem item) {
 		itens.add(item);
 	}
@@ -24,5 +25,17 @@ public class CarrinhoCompras implements Serializable{
 	public List<CarrinhoItem> getItens() {
 		return new ArrayList<CarrinhoItem>(itens);
 	}
+
+	public BigDecimal getTotal(CarrinhoItem item) {
+		return item.getLivro().getPreco().multiply(new BigDecimal(item.getQuantidade()));
+	}
 	
+	public BigDecimal getTotal() {
+		BigDecimal total = BigDecimal.ZERO;
+		for (CarrinhoItem carrinhoItem : itens) {
+			total = total.add(carrinhoItem.getLivro().getPreco().multiply(new BigDecimal(carrinhoItem.getQuantidade())));
+		}
+		return total;
+	}
+
 }
