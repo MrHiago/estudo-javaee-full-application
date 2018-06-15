a5b3089a134696fb080f9bf1ddac3606974642c3
Hiago Miranda Ribeiro <hiagomiranda95@gmail.com>
2018-06-15 16:09:17 -0300

    Colocando cache no jsf com omnifaces

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
@@ -3,10 +3,12 @@ package br.com.casadocodigo.loja.daos;
 import java.util.List;
 
 import javax.ejb.Stateful;
+import javax.persistence.Cache;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
 
+import org.hibernate.SessionFactory;
 import org.hibernate.jpa.QueryHints;
 
 import br.com.casadocodigo.loja.models.Livro;
@@ -33,6 +35,7 @@ public class LivroDao {
 		return manager.createQuery(jpql, Livro.class)
 				.setMaxResults(5)
 				.setHint(QueryHints.HINT_CACHEABLE, true)
+				.setHint(QueryHints.HINT_CACHE_REGION, "home")
 				.getResultList();
 	}
 
@@ -40,13 +43,12 @@ public class LivroDao {
 		String jpql = "select l from Livro l order by l.id desc";
 		return manager.createQuery(jpql, Livro.class)
 				.setFirstResult(5)
-				.setHint(QueryHints.HINT_CACHEABLE, true) 
+				.setHint(QueryHints.HINT_CACHEABLE, true)
+				.setHint(QueryHints.HINT_CACHE_REGION, "home")
 				.getResultList();
 	}
 
 	public Livro buscarPorId(Integer id) {
-//		return manager.find(Livro.class, id);
-		
 		String jpql = "select l from Livro l join fetch l.autores "
 				+ "where l.id = :id";
 		return manager.createQuery(jpql, Livro.class)
@@ -54,15 +56,14 @@ public class LivroDao {
 				.getSingleResult();
 	}
 	
+	public void limpaCache() {
+		Cache cache = this.manager.getEntityManagerFactory().getCache();
+		cache.evict(Livro.class);
+		cache.evictAll();
+		
+		SessionFactory factory = this.manager.getEntityManagerFactory().unwrap(SessionFactory.class);
+		factory.getCache().evictAllRegions();
+		factory.getCache().evictQueryRegion("home");
+	}
+	
 }
-
-
-
-
-
-
-
-
-
-
-
